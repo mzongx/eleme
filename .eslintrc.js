74a0cf07d59df988e8761eff6ca3974cf7d6bd4d
mzongx <1052036428@qq.com>
2018-04-20 11:44:47 +0800

    add remark in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow function-paren 0
     'space-before-function-paren': 0
   }
 }
